ec0b8583dbd7b2061422e05f1df99ac072d292a3
josly15 <rohan.mathias@gmail.com>
2024-10-11 12:50:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def sentiment_analysis(input):
     user_sequences = tokenizer.texts_to_sequences([input])
     user_sequences_matrix = sequence.pad_sequences(user_sequences, maxlen=1225)
     prediction = model.predict(user_sequences_matrix)
-    return round(float(prediction[0][0]),2)
+    return round(float(prediction[0][0]) * 100, 2)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
